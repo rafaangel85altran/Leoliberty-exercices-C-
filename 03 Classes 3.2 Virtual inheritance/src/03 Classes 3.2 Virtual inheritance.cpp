a54e6bd6aedba3fed5e2dbfa045ae18c28fe9de5
jrangel_segula <jrangel@segula.es>
2019-05-15 15:12:33 +0200

    Will I go to the scafold induction to villanueva del gallego?

---
+++ 03 Classes 3.2 Virtual inheritance/src/03 Classes 3.2 Virtual inheritance.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 2.cpp
+// Author      : jrangel
+// Version     :
+// Copyright   : Module 3. 3.2 Virtual inheritance
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "http://www.ciens.ucv.ve/algoritmosyprogramacion/documentos/LeoLiberti_IntroduccionC%2B%2B_Exercises_Solutions_Lix.pdf" << endl; // prints http://www.ciens.ucv.ve/algoritmosyprogramacion/documentos/LeoLiberti_IntroduccionC%2B%2B_Exercises_Solutions_Lix.pdf
+	return 0;
+}
