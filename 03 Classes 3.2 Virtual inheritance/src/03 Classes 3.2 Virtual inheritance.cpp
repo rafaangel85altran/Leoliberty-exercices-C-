ae058c63c822749f6d215b22e74683d9e0745c1d
jrangel_segula <jrangel@segula.es>
2019-05-15 15:37:22 +0200

    I start to understand more or less the code

--- 03 Classes 3.2 Virtual inheritance/src/03 Classes 3.2 Virtual inheritance.cpp
+++ 03 Classes 3.2 Virtual inheritance/src/03 Classes 3.2 Virtual inheritance.cpp
@@ -6,10 +6,47 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+// this program does not compile!
+#include<iostream>
 
-int main() {
-	cout << "http://www.ciens.ucv.ve/algoritmosyprogramacion/documentos/LeoLiberti_IntroduccionC%2B%2B_Exercises_Solutions_Lix.pdf" << endl; // prints http://www.ciens.ucv.ve/algoritmosyprogramacion/documentos/LeoLiberti_IntroduccionC%2B%2B_Exercises_Solutions_Lix.pdf
-	return 0;
-}
+class VirtualBase {
+	public:
+		virtual ~VirtualBase() { }
+		virtual void setValue(int i) = 0;
+		virtual int getValue(void) = 0;
+	};
+
+class Derived : public virtual VirtualBase {
+	public:
+		~Derived() { }
+		void setValue(int i) {
+			theValue = i;
+			}
+		int getValue(void) {
+			return theValue;
+			}
+		void printValue(void) {
+			std::cout << "Derived::printValue(): value is " << theValue << std::endl;
+			}
+	private:
+		int theValue;
+	};
+
+void printTheValue1(VirtualBase* v) {
+	std::cout << "printTheValue1(): value is " << v->getValue() << std::endl;
+	}
+
+void printTheValue2(VirtualBase* v) {
+	Derived* d = dynamic_cast<Derived*>(v);
+	d->printValue();
+	}
+
+int main(int argc, char** argv) {
+	int ret = 0;
+	Derived d;
+	VirtualBase* v = &d;
+	v->setValue(1);
+	printTheValue1(v);
+	printTheValue2(v);
+	return ret;
+	}
