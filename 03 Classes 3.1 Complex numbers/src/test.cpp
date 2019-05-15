d4c2e9e7748605823be506caa30a4c5b6f5eb702
jrangel_segula <jrangel@segula.es>
2019-05-15 15:09:59 +0200

    I managed to convert the code so I can introduce the complex numbers by console

--- 03 Classes 3.1 Complex numbers/src/test.cpp
+++ 03 Classes 3.1 Complex numbers/src/test.cpp
@@ -12,7 +12,7 @@
 
 int main(int argc, char** argv) {
 using namespace std;
-
+/*
 if (argc < 4) {
 	cerr << "need an operation on command line" << endl;
 	cerr << " e.g. ./test 4.3+3i - 2+3.1i" << endl;
@@ -20,10 +20,20 @@ if (argc < 4) {
 	cerr << " separate the operands and the operator - use arithmetic\n";
 	cerr << " operators only)" << endl;
 	return 1;
-	}
+	}*/
 
-string complexString1 = argv[1];
-string complexString2 = argv[3];
+string complexString1;
+string complexString2;
+char operation;
+
+cout << "Enter first complex number" << endl;
+cin >> complexString1;
+
+cout << "Enter second complex number" << endl;
+cin >> complexString2;
+
+cout << "Enter operation" << endl;
+cin >> operation;
 
 Complex complex1;
 complex1.fromString(complexString1);
@@ -31,16 +41,16 @@ Complex complex2;
 complex2.fromString(complexString2);
 Complex complex3;
 
-	if (argv[2][0] == '+') {
+	if (operation == '+') {
 		complex3 = complex1 + complex2;
-	} else if (argv[2][0] == '-') {
+	} else if (operation == '-') {
 		complex3 = complex1 - complex2;
-	} else if (argv[2][0] == '*' || argv[2][0] == '.') {
-	argv[2][0] = '*';
+	} else if (operation == '*' || operation == '.') {
+		operation = '*';
 		complex3 = complex1 * complex2;
-	} else if (argv[2][0] == '/') {
+	} else if (operation == '/') {
 		complex3 = complex1 / complex2;}
-	cout << complex1 << " " << argv[2][0] << " (" << complex2 << ") = " << complex3 << endl;
+	cout << "(" << complex1 << ") " << operation << " (" << complex2 << ") = " << complex3 << endl;
 
 return 0;
 }
